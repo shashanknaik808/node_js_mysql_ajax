0dd6564e22ee687f3cc9bc5284ba3ddca317e621
Shashank Naik <shashanknaik808@gmail.com>
2023-10-05 11:39:42 +0530

    Inserting Values using AJAX

--- controllers/students_controllers.js
+++ controllers/students_controllers.js
@@ -5,3 +5,26 @@ const connection = require('../model/connection.js');
 module.exports.homePage = (req, res) => {
     res.sendFile(__dirname + '/register.html');
 };
+
+
+module.exports.insertValues = (req, res) => {
+    console.log(req.body);
+
+
+    const { name, email, mobile } = req.body;
+
+    let sql_query = `INSERT INTO STUDENTS (NAME, EMAIL, MOBILE) VALUES ?;`;
+
+    let values = [
+        [name, email, mobile]
+    ];
+
+    connection.query(sql_query, [values], (err, result) => {
+        if (err) return console.log("Error while inserted data " + err);
+        res.send("Data inserted successfull");
+
+        
+    });
+
+
+}
--- routes/students_routes.js
+++ routes/students_routes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 
-const { homePage } = require('../controllers/students_controllers.js');
+const { homePage, insertValues } = require('../controllers/students_controllers.js');
 
 const studentRouter = express.Router();
 
 studentRouter.get('/', homePage);
+studentRouter.post('/', insertValues);
 
 
 module.exports = studentRouter;
